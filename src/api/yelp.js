7fefcb2cbabc302a4ca7da20a04cdb7af59eccf4
Androzelos <vejdetcubukcu@hotmail.com>
2025-04-08 19:13:31 +0300

    Add yelp.js to .gitignore and update yelp.js with a comment

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -6,3 +6,5 @@ export default axios.create({
         Authorization: "Bearer Q0NWfm-95cLU82-D0N6_0i89nG_k5xiKLM1Gq8P5kLqc_ZGEf-GGywfuY0Kz35odIbwwcpwIoRZTJwhHs1ci9B1L5mr5CVlk5Yd8U6cogKIQlvKGgXed-cjv9xDQZ3Yx"
     }
 })
+
+//woaw
